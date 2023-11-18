70f12386ef16a5c4e89c7b421dd9978e7934ad24
mattsullentrup <msullentrup1@gmail.com>
2023-11-17 19:19:16 -0600

    button color switch

--- productivity_app/scripts/main.gd
+++ productivity_app/scripts/main.gd
@@ -1,13 +1,18 @@
 extends Node
 
 
+@export var button : Button
+
 @onready var timer = %Timer
 @onready var timer_label = %TimerLabel
 @onready var timer_message = %TimerMessage
+@onready var button_colors = [Color.RED, Color.YELLOW, Color.GREEN]
+@onready var current_button_color : int = 1
 
 
 func _ready() -> void:
 	timer_message.hide()
+	button.self_modulate = button_colors[0]
 
 
 func _process(_delta: float) -> void:
@@ -21,3 +26,8 @@ func _on_timer_button_pressed() -> void:
 
 func _on_timer_timeout() -> void:
 	timer_message.show()
+
+
+func _on_button_pressed() -> void:
+	button.self_modulate = button_colors[current_button_color % 3]
+	current_button_color += 1
