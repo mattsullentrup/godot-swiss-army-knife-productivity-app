38299265a7cfd8bdf09195901c3c4d3309e1bd57
mattsullentrup <msullentrup1@gmail.com>
2024-01-21 16:15:32 -0600

    Reorganize and refactor tasks

--- productivity_app/scripts/task_manager.gd
+++ productivity_app/scripts/task_manager.gd
@@ -1,20 +1,20 @@
 extends PanelContainer
 
 
-@export var task_button: Button
-@export var task_button_2: Button
+@export var task_buttons = []
 
 @onready var button_colors = [Color.RED, Color.YELLOW, Color.GREEN]
 @onready var current_button_color: int = 1
 
-var task_buttons = []
-
 
 func _ready() -> void:
-	task_button.self_modulate = button_colors[0]
-	task_button_2.self_modulate = button_colors[0]
+	for child in $VBoxContainer.get_children():
+		var button = child.get_child(0)
+		button.self_modulate = button_colors[0]
+		button.pressed.connect(_on_task_button_pressed.bind(button))
 
 
-func _on_task_button_pressed() -> void:
-	task_button.self_modulate = button_colors[current_button_color % 3]
+func _on_task_button_pressed(button: Button):
+	button.self_modulate = button_colors[current_button_color % 3]
 	current_button_color += 1
+
