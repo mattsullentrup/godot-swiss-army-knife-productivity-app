96373fb29d3db5293dfce8d38c291cc9ce5e3502
mattsullentrup <msullentrup1@gmail.com>
2024-01-25 18:49:00 -0600

    Move timer code to dedicated timer_manager script

--- productivity_app/scripts/main.gd
+++ productivity_app/scripts/main.gd
@@ -1,33 +1,4 @@
 extends Node
 
 
-@onready var timer = %Timer
-@onready var timer_label = %TimerLabel
-@onready var timer_message = %TimerMessage
-@onready var timer_length: int = (%TimeOptionButton.selected + 1) * 5
 
-
-func _ready() -> void:
-	timer_message.hide()
-
-
-func _process(_delta: float) -> void:
-	timer_label.text = type_convert(ceilf(timer.time_left), TYPE_STRING)
-
-
-func _on_timer_button_pressed() -> void:
-	timer.stop()
-	timer.start(timer_length)
-	timer_message.hide()
-
-
-func _on_timer_timeout() -> void:
-	timer_message.show()
-
-
-func _on_time_option_button_item_selected(index: int) -> void:
-	timer_length = (index + 1) * 5
-
-
-func _on_timer_stop_button_pressed() -> void:
-	timer.stop()
