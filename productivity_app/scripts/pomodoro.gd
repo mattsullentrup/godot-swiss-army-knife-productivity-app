4707e0df86aa3c10101a6082619dfb33c24d192e
mattsullentrup <msullentrup1@gmail.com>
2024-01-26 13:08:12 -0600

    Create enum for pomodoro states

---
+++ productivity_app/scripts/pomodoro.gd
@@ -0,0 +1,14 @@
+extends TabBar
+
+
+enum Round {
+	FIRST = 1,
+	SECOND = 2,
+	THIRD = 3,
+	FOURTH = 4,
+}
+
+@export var pomodoro_label: Label
+
+func _ready() -> void:
+	pomodoro_label.text = str(Round.FIRST) + '/4'
