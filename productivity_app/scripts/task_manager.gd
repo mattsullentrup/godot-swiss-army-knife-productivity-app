debd6871903b4f542452e99e325b416f57d2855a
mattsullentrup <msullentrup1@gmail.com>
2024-01-30 17:35:43 -0600

    Fix untyped errors

--- productivity_app/scripts/task_manager.gd
+++ productivity_app/scripts/task_manager.gd
@@ -1,20 +1,20 @@
 extends PanelContainer
 
 
-@export var task_buttons = []
+@export var task_buttons : Array[Button] = []
 
-@onready var button_colors = [Color.RED, Color.YELLOW, Color.GREEN]
+@onready var button_colors : Array[Color] = [Color.RED, Color.YELLOW, Color.GREEN]
 @onready var current_button_color: int = 1
 
 
 func _ready() -> void:
-	for child in $VBoxContainer.get_children():
-		var button = child.get_child(0)
+	for child: HBoxContainer in $VBoxContainer.get_children():
+		var button: Button = child.get_child(0)
 		button.self_modulate = button_colors[0]
 		button.pressed.connect(_on_task_button_pressed.bind(button))
 
 
-func _on_task_button_pressed(button: Button):
+func _on_task_button_pressed(button: Button) -> void:
 	button.self_modulate = button_colors[current_button_color % 3]
 	current_button_color += 1
 
