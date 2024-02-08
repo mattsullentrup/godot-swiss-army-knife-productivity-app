04ded00ef8b0634f0c1f9c8efb9987490794a88e
mattsullentrup <msullentrup1@gmail.com>
2024-02-07 19:51:49 -0600

    Change task manager script to change type variation instead of just color

--- productivity_app/scripts/task_manager.gd
+++ productivity_app/scripts/task_manager.gd
@@ -1,31 +1,23 @@
 extends PanelContainer
 
 
-@export var task_buttons : Array[Button] = []
 @export var task : PackedScene
 
-@onready var button_colors : Array[Color] = [Color.RED, Color.YELLOW, Color.GREEN]
+#@onready var button_colors : Array[Color] = [Color.RED, Color.YELLOW, Color.GREEN]
+@onready var button_colors : Array[StringName] = [&"RedButton", &"YellowButton", &"GreenButton"]
 @onready var current_button_color : int = 1
 
 
-func _ready() -> void:
-	#for child: HBoxContainer in $VBoxContainer.get_children():
-		#var button: Button = child.get_child(0)
-		#button.self_modulate = button_colors[0]
-		#button.pressed.connect(_on_task_button_pressed.bind(button))
-		pass
-
-
 func _on_task_button_pressed(button: Button) -> void:
-	button.self_modulate = button_colors[current_button_color % 3]
+	button.theme_type_variation = button_colors[current_button_color % 3]
 	current_button_color += 1
-
 
 
 func _on_new_task_button_pressed() -> void:
 	var new_task : HBoxContainer = task.instantiate()
 	var task_button : Button = new_task.get_child(0)
-	task_button.self_modulate = button_colors[0]
+
+	task_button.theme_type_variation = button_colors[0]
 	task_button.pressed.connect(_on_task_button_pressed.bind(task_button))
 
 	var delete_button : Button = new_task.get_child(2)
