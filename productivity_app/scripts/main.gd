4bbfd87ffea4435c05c531febe52406cad036cb2
mattsullentrup <msullentrup1@gmail.com>
2024-02-20 14:40:15 -0600

    Refactor task code

--- productivity_app/scripts/main.gd
+++ productivity_app/scripts/main.gd
@@ -1,7 +1,53 @@
 extends Node
 
 
-#@onready var clock_label: Label = %ClockLabel
+const SAVE_PATH = "user://save_config_file.ini"
+
+## The root game node (so we can get and instance enemies).
+@export var task_node: NodePath
+## The player node (so we can set/get its health and position).
+@export var pomodoro_node: NodePath
+
+
+func save_game() -> void:
+	var config := ConfigFile.new()
+
+	var pomodoro := get_node(pomodoro_node) as Pomodoro
+	config.set_value("pomodoro", "current_state", pomodoro.current_state)
+	config.set_value("pomodoro", "current_round", pomodoro.current_round)
+	config.set_value("pomodoro", "productivity_state", pomodoro.productivity_state)
+
+	var tasks := []
+	for task in get_tree().get_nodes_in_group(&"task"):
+		tasks.push_back({
+			position = task.position,
+		})
+	config.set_value("tasks", "tasks", tasks)
+
+	config.save(SAVE_PATH)
+
+	#(get_node(^"../LoadConfigFile") as Button).disabled = false
+
+
+func load_game() -> void:
+	var config := ConfigFile.new()
+	config.load(SAVE_PATH)
+
+	var pomodoro := get_node(pomodoro_node) as Pomodoro
+	pomodoro.current_state = config.get_value("pomodoro", "current_state")
+	pomodoro.current_round = config.get_value("pomodoro", "current_round")
+	pomodoro.productivity_state = config.get_value("pomodoro", "productivity_state")
+
+	# Remove existing enemies before adding new ones.
+	get_tree().call_group("task", "queue_free")
+
+	var tasks = config.get_value("tasks", "tasks")
+	var task_manager : PanelContainer = get_node(task_node)
+
+	for task_config : Task in tasks:
+		var task := preload("res://scenes/task.tscn").instantiate() as Task
+		#enemy.position = enemy_config.position
+		task_manager.add_child(task)
 
 
 
--- productivity_app/scripts/task_manager.gd
+++ productivity_app/scripts/task_manager.gd
@@ -3,28 +3,8 @@ extends PanelContainer
 
 @export var task : PackedScene
 
-#@onready var button_colors : Array[Color] = [Color.RED, Color.YELLOW, Color.GREEN]
-@onready var button_colors : Array[StringName] = [&"RedButton", &"YellowButton", &"GreenButton"]
-@onready var current_button_color : int = 1
-
-
-func _on_task_button_pressed(button: Button) -> void:
-	button.theme_type_variation = button_colors[current_button_color % 3]
-	current_button_color += 1
-
 
 func _on_new_task_button_pressed() -> void:
 	var new_task : HBoxContainer = task.instantiate()
-	var task_button : Button = new_task.get_child(0)
-
-	task_button.theme_type_variation = button_colors[0]
-	task_button.pressed.connect(_on_task_button_pressed.bind(task_button))
-
-	var delete_button : Button = new_task.get_child(2)
-	delete_button.pressed.connect(_on_delete_button_pressed.bind(delete_button))
-
+	new_task.task_manager = self
 	$TaskVBoxContainer.add_child(new_task)
-
-
-func _on_delete_button_pressed(button: Button) -> void:
-	button.get_parent().queue_free()
