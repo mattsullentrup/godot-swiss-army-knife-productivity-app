e93e2745d8cc0ef8a4e4232d5938bb6168c4975d
mattsullentrup <msullentrup1@gmail.com>
2024-02-20 13:48:58 -0600

    Add date and weekday

--- productivity_app/scripts/time.gd
+++ productivity_app/scripts/time.gd
@@ -1,42 +1,62 @@
-extends HBoxContainer
+extends Control
 
 
 @onready var clock_label : Label = $ClockLabel
 @onready var date_label : Label = $DateLabel
+@onready var weekday_label: Label = $WeekdayLabel
 
-var datetime
+var datetime : Dictionary
 
 
 func _process(_delta: float) -> void:
-	datetime = Time.get_datetime_string_from_system()
+	#datetime = Returns the current date as a dictionary of keys:
+	#year, month, day, weekday, hour, minute, second, and dst (Daylight Savings Time).
+	datetime = Time.get_datetime_dict_from_system()
 	get_time()
 	get_date()
+	weekday_label.text = get_weekday()
+
+
+func get_weekday() -> String:
+	var weekday : String
+
+	match datetime.get("weekday"):
+		0:
+			weekday = "Sunday"
+		1:
+			weekday = "Monday"
+		2:
+			weekday = "Tuesday"
+		3:
+			weekday = "Wednesday"
+		4:
+			weekday = "Thursday"
+		5:
+			weekday = "Friday"
+		0:
+			weekday = "Saturday"
+
+	return weekday
 
 
 func get_date() -> void:
-	var date := Time.get_datetime_string_from_system()
-	date_label.text = date
+	date_label.text = str(
+			datetime.get("month"), "/",
+			datetime.get("day"), "/",
+			datetime.get("year")
+	)
 
 
 func get_time() -> void:
+	var hour : int = datetime.get("hour")
+
 	# Convert to american time
-	var time := Time.get_time_string_from_system()
-	var hour := str(time[0] + time[1])
-	var int_hour := int(hour)
+	if hour > 12:
+		hour -= 12
 
-	if int_hour > 12:
-		int_hour -= 12
-		var converted_hour := str(int_hour)
-
-		# Check if it's before 10 PM
-		if int_hour < 10:
-			time[0] = "0"
-			time[1] = converted_hour[0]
-		else:
-			time[0] = converted_hour[0]
-			time[1] = converted_hour[1]
-
-		clock_label.text = time + " PM"
-	else:
-		clock_label.text = time + " AM"
+	clock_label.text = str(
+			hour, ":",
+			datetime.get("minute"), ":",
+			datetime.get("second"), " PM"
+	)
 
