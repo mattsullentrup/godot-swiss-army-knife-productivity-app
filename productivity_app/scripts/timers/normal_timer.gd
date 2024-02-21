ca9df9096a8d3a5ada589115f08fe5cfc931dc8c
mattsullentrup <msullentrup1@gmail.com>
2024-02-21 13:52:26 -0600

    Add notification sound to normal timer

--- productivity_app/scripts/timers/normal_timer.gd
+++ productivity_app/scripts/timers/normal_timer.gd
@@ -3,14 +3,10 @@ extends TabBar
 
 @export var normal_timer : Timer
 @export var normal_time_remaining_label : Label
-@export var normal_timer_message : Label
 @export var normal_timer_option_button : OptionButton
 
-@onready var normal_timer_length: int = (normal_timer_option_button.selected + 1) * 5
-
-
-func _ready() -> void:
-	normal_timer_message.hide()
+@onready var normal_timer_length : int = (normal_timer_option_button.selected + 1) * 5
+@onready var notification_sound : AudioStreamPlayer = %NotificationSound
 
 
 func _process(_delta: float) -> void:
@@ -20,7 +16,6 @@ func _process(_delta: float) -> void:
 func _on_normal_timer_start_button_pressed() -> void:
 	normal_timer.stop()
 	normal_timer.start(normal_timer_length)
-	normal_timer_message.hide()
 
 
 func _on_normal_timer_stop_button_pressed() -> void:
@@ -28,7 +23,7 @@ func _on_normal_timer_stop_button_pressed() -> void:
 
 
 func _on_normal_timer_timeout() -> void:
-	normal_timer_message.show()
+	notification_sound.play()
 
 
 func _on_normal_time_option_button_item_selected(index: int) -> void:
