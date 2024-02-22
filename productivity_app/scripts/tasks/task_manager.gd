3dbd706bab81a6fcd308db37a095823b71f4c37c
mattsullentrup <msullentrup1@gmail.com>
2024-02-22 16:20:17 -0600

    Add task reset button

--- productivity_app/scripts/tasks/task_manager.gd
+++ productivity_app/scripts/tasks/task_manager.gd
@@ -7,3 +7,9 @@ extends VBoxContainer
 func _on_new_task_button_pressed() -> void:
 	var new_task : HBoxContainer = task.instantiate()
 	add_child(new_task)
+
+
+func _on_reset_button_pressed() -> void:
+	var children : Array = get_children()
+	for child : Node in children:
+		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
