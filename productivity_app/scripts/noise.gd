2def8740cc3fd9838b5cd7a721c7d32e7bd99145
mattsullentrup <msullentrup1@gmail.com>
2024-02-23 10:58:27 -0600

    Add volume slider for pink noise

--- productivity_app/scripts/noise.gd
+++ productivity_app/scripts/noise.gd
@@ -2,9 +2,18 @@ extends VBoxContainer
 
 
 @onready var pink_noise: AudioStreamPlayer = %PinkNoise
+@onready var volume_h_slider: HSlider = %VolumeHSlider
 
+
+func _ready() -> void:
+	pink_noise.volume_db = linear_to_db(volume_h_slider.value)
 
 
 func _on_noise_button_toggled(toggled_on: bool) -> void:
 	toggled_on = not toggled_on
 	pink_noise.playing = toggled_on
+
+
+func _on_volume_h_slider_value_changed(value: float) -> void:
+	# Use `linear_to_db()` to get a volume slider that matches perceptual human hearing.
+	pink_noise.volume_db = linear_to_db(value)
