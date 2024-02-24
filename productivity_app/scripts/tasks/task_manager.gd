89d6e3d76ea0258ec5a19fe4398889f6e4e461c8
mattsullentrup <msullentrup1@gmail.com>
2024-02-24 12:33:41 -0600

    Fix task reset button

--- productivity_app/scripts/tasks/task_manager.gd
+++ productivity_app/scripts/tasks/task_manager.gd
@@ -12,4 +12,5 @@ func _on_new_task_button_pressed() -> void:
 func _on_reset_button_pressed() -> void:
 	var children : Array = get_children()
 	for child : Node in children:
+		child.current_button_color = 0
 		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
