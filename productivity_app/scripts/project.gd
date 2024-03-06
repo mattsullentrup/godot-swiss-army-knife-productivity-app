a9fd85b19761f0e7c98995086334c329e32033ac
mattsullentrup <msullentrup1@gmail.com>
2024-03-06 16:07:49 -0600

    Add reset button pressed method to project script

---
+++ productivity_app/scripts/project.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+
+func _on_reset_button_pressed() -> void:
+	var children : Array = get_children()
+	for child : Node in children:
+		child.current_button_color = 0
+		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
