622a8e7acc8bd947d679796e87c3585d46b15841
mattsullentrup <msullentrup1@gmail.com>
2024-03-06 16:09:03 -0600

    Remove reset projects button

--- productivity_app/scripts/tasks/task_manager.gd
+++ productivity_app/scripts/tasks/task_manager.gd
@@ -7,10 +7,3 @@ extends VBoxContainer
 func _on_new_project_button_pressed() -> void:
 	var new_project : HBoxContainer = project.instantiate()
 	add_child(new_project)
-
-
-func _on_reset_button_pressed() -> void:
-	var children : Array = get_children()
-	for child : Node in children:
-		child.current_button_color = 0
-		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
