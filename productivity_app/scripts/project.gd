c5693df0a4d32ee85cafb26b8491b2c09d53be60
mattsullentrup <msullentrup1@gmail.com>
2024-03-06 16:18:13 -0600

    Add new task method

--- productivity_app/scripts/project.gd
+++ productivity_app/scripts/project.gd
@@ -1,8 +1,16 @@
 extends HBoxContainer
 
 
+@export var task : PackedScene
+
+
 func _on_reset_button_pressed() -> void:
 	var children : Array = get_children()
 	for child : Node in children:
 		child.current_button_color = 0
 		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
+
+
+func _on_new_task_button_pressed() -> void:
+	var new_task: HBoxContainer = task.instantiate()
+	add_child(new_task)
