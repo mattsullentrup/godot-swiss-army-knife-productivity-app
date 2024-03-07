d4254074787989e13caa9c386715343588d65074
mattsullentrup <msullentrup1@gmail.com>
2024-03-06 18:24:34 -0600

    Add delete and reset button signal functions to project

--- productivity_app/scripts/tasks/project.gd
+++ productivity_app/scripts/tasks/project.gd
@@ -1,11 +1,5 @@
 extends VBoxContainer
 
 
-@export var task : PackedScene
-
-
-func _on_reset_button_pressed() -> void:
-	var children : Array = get_children()
-	for child : Node in children:
-		child.current_button_color = 0
-		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
+func _on_delete_button_pressed() -> void:
+	queue_free()
--- productivity_app/scripts/tasks/task_container.gd
+++ productivity_app/scripts/tasks/task_container.gd
@@ -16,7 +16,10 @@ func _on_toggle_tasks_button_pressed() -> void:
 		for child in children:
 			child.hide()
 		are_children_visible = false
-	else:
-		for child in children:
-			child.show()
-		are_children_visible = true
+
+
+func _on_reset_button_pressed() -> void:
+	var children : Array = get_children()
+	for child : Node in children:
+		child.current_button_color = 0
+		child.get_node("TaskStateButton").theme_type_variation = child.button_types[0]
