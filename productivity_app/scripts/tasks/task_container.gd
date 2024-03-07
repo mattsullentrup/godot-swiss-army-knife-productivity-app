c37587db1a37c1225c07697db41517e8d1d262b6
mattsullentrup <msullentrup1@gmail.com>
2024-03-06 18:16:30 -0600

    Add hide children toggle button to project

--- productivity_app/scripts/tasks/task_container.gd
+++ productivity_app/scripts/tasks/task_container.gd
@@ -6,13 +6,17 @@ var are_children_visible := true
 
 
 func _on_new_task_button_pressed() -> void:
-    var new_task: HBoxContainer = task.instantiate()
-    add_child(new_task)
+	var new_task: HBoxContainer = task.instantiate()
+	add_child(new_task)
 
 
 func _on_toggle_tasks_button_pressed() -> void:
-    var children : Array[Node] = get_children()
-    if are_children_visible:
-        for child in children:
-            child.hide()
-        are_children_visible = false
+	var children : Array[Node] = get_children()
+	if are_children_visible:
+		for child in children:
+			child.hide()
+		are_children_visible = false
+	else:
+		for child in children:
+			child.show()
+		are_children_visible = true
