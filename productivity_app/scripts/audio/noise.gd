a2dc0245f267a204e902f7f60820c5ae4bc8115b
mattsullentrup <msullentrup1@gmail.com>
2024-03-07 20:09:12 -0600

    Change noise button to icon

--- productivity_app/scripts/audio/noise.gd
+++ productivity_app/scripts/audio/noise.gd
@@ -1,8 +1,12 @@
 extends VBoxContainer
 
 
+@export var audio_off_icon : CompressedTexture2D
+@export var audio_on_icon : CompressedTexture2D
+
 @onready var pink_noise : AudioStreamPlayer = %PinkNoise
 @onready var volume_h_slider : HSlider = %VolumeHSlider
+@onready var noise_button : Button = %NoiseButton
 
 
 func _ready() -> void:
@@ -20,6 +24,7 @@ func _on_noise_button_toggled(toggled_on: bool) -> void:
 	# pink_noise.playing = toggled_on
 	toggled_on = not toggled_on
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Noise"), toggled_on)
+	noise_button.icon = audio_on_icon if not toggled_on else audio_off_icon
 
 
 func _on_volume_h_slider_value_changed(value: float) -> void:
