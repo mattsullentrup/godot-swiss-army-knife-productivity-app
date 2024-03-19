39efb2cedb4ab4579401bd5f6fc62ff484cce449
mattsullentrup <msullentrup1@gmail.com>
2024-03-18 23:55:02 -0500

    Get autopan fully working

--- productivity_app/scripts/audio/effects/autopan.gd
+++ productivity_app/scripts/audio/effects/autopan.gd
@@ -1,47 +1,38 @@
 extends HBoxContainer
 
 
-@export var phaser_manager : HBoxContainer
+@export var pan_speed : float = 0.0005
 
 var can_autopan := false
-var pan_strength : float = 1
-var pan_speed : float = 0.05
-var pan_value : float = 0
-
 var panner_index : int = 1
 
-@onready var effects_bus := AudioServer.get_bus_index("Effects")
-@onready var panner := AudioServer.get_bus_effect(effects_bus, 1) as AudioEffectPanner
+@onready var effects_bus_index := AudioServer.get_bus_index("Effects")
+@onready var noise_bus_index := AudioServer.get_bus_index("Noise")
+@onready var panner := AudioServer.get_bus_effect(effects_bus_index, 1) as AudioEffectPanner
 
 
 func _ready() -> void:
-	AudioServer.set_bus_effect_enabled(effects_bus, panner_index, false)
-
-	can_autopan = true
+	AudioServer.set_bus_effect_enabled(effects_bus_index, panner_index, false)
 
 
 func _process(_delta: float) -> void:
-	pass
-	#if can_autopan:
-		#if is_equal_approx(pan_value, pan_strength):
-			#pan_speed = -pan_speed
-		#elif is_equal_approx(pan_value, -pan_strength):
-			#pan_speed = abs(pan_speed)
-		#pan_value += 0.1 * pan_speed
-		#clampf(pan_value, -1, 1)
-		#panner.pan = pan_value
-		#print(panner.pan)
-
-
-
+	if can_autopan and not AudioServer.is_bus_mute(noise_bus_index):
+		if panner.pan > 1:
+			pan_speed = -pan_speed
+		if panner.pan < -1:
+			pan_speed = abs(pan_speed)
+		panner.pan += pan_speed
+		clampf(panner.pan, -1, 1)
+		print(panner.pan)
 
 
 func _on_autopan_check_button_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_effect_enabled(effects_bus, panner_index, toggled_on)
+	AudioServer.set_bus_effect_enabled(effects_bus_index, panner_index, toggled_on)
 	toggled_on = not toggled_on
 	can_autopan = not can_autopan
+	if can_autopan == false:
+		panner.pan = 0
 
 
 func _on_autopan_slider_value_changed(value: float) -> void:
-	pan_strength = value
-	#pan_value = 0
+	pan_speed = value
