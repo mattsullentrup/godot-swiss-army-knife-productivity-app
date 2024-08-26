779ff19045f7d5b23afee6a4a7c86f02a0d450a9
mattsullentrup <msullentrup1@gmail.com>
2024-08-26 09:58:37 -0500

    Work on tree task setup

--- productivity_app/scripts/tasks/tree.gd
+++ productivity_app/scripts/tasks/tree.gd
@@ -1,37 +1,40 @@
-@tool
 extends Tree
 
 
 func _ready() -> void:
-	var root: TreeItem = create_item()
-	root.set_text(0, "Tree - Root")
-	allow_reselect = true
-	#var child1: TreeItem = create_item(root)
-	#child1.set_text(0, "Tree - Child 1")
-	#var child2: TreeItem = create_item(root)
-	#child2.set_text(0, "Tree - Child 2")
-	#var subchild1: TreeItem = create_item(child1)
-	#subchild1.set_text(0, "Tree - Subchild 1")
+	var tree := Tree.new()
+	var root: TreeItem = tree.create_item()
+	#tree.hide_root = true
 
-	var new_task: TreeItem = create_item(root)
+	var child1: TreeItem = tree.create_item(root)
+	child1.set_text(0, "Child1")
+
+	var child2: TreeItem = tree.create_item(root)
+	var subchild1: TreeItem = tree.create_item(child1)
+
+	#var new_task: TreeItem = create_item(root)
 	#root.add_child(new_task)
 
 	# Allow text to be changed
-	new_task.set_editable(1, true)
-	new_task.set_text(1, "Tree - Child 1")
+	#new_task.set_editable(1, true)
+	#new_task.set_text(1, "Tree - Child 1")
 
-	new_task.set_cell_mode(0, TreeItem.CELL_MODE_CHECK)
+	#new_task.set_cell_mode(0, TreeItem.CELL_MODE_CHECK)
 
 
-func _on_item_selected() -> void:
-	var cell: TreeItem = get_selected()
+#func _on_item_selected() -> void:
+	#var cell: TreeItem = get_selected()
+#
+	#if not cell.is_selected(0):
+		#return
+#
+	#if cell.is_checked(0):
+		#cell.set_checked(0, false)
+	#elif cell.is_indeterminate(0):
+		#cell.set_checked(0, true)
+	#else:
+		#cell.set_indeterminate(0, true)
 
-	if not cell.is_selected(0):
-		return
 
-	if cell.is_checked(0):
-		cell.set_checked(0, false)
-	elif cell.is_indeterminate(0):
-		cell.set_checked(0, true)
-	else:
-		cell.set_indeterminate(0, true)
+func _on_new_project_button_pressed() -> void:
+	pass # Replace with function body.
