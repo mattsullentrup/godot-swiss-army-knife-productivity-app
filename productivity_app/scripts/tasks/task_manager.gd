5c7b0f2a99e35262478d4bff04b6b6cd8f90d887
mattsullentrup <msullentrup1@gmail.com>
2024-09-01 17:25:47 -0500

    Delete task_manager.gd

--- productivity_app/scripts/tasks/task_manager.gd
+++
@@ -1,9 +0,0 @@
-extends VBoxContainer
-
-
-@export var project: PackedScene
-
-
-func _on_new_project_button_pressed() -> void:
-	var new_project: VBoxContainer = project.instantiate()
-	add_child(new_project)
