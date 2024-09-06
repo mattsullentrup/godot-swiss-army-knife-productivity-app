1528b8146e4a3b7f3fa82ae45d5cac6f122186b8
mattsullentrup <msullentrup1@gmail.com>
2024-09-06 18:41:47 -0500

    Give all states a class name

--- productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
+++ productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
@@ -1,13 +1,14 @@
+class_name IdleState
 extends State
 
 
 func _enter(_previous_state: State) -> void:
 	super(_previous_state)
 	state_machine.pomodoro_timer.stop()
-	get_parent().time_to_display = 0
+	state_machine.time_to_display = 0
 
 
 #func _on_button_pressed(button: PomodoroStateMachine.ButtonTypes) -> void:
 func _on_button_pressed(button: int) -> void:
-	if button == PomodoroStateMachine.ButtonTypes.START:
+	if button == state_machine.ButtonTypes.START:
 		finished.emit("Work")
--- productivity_app/common/timers/pomodoro_state_machine/states/paused.gd
+++ productivity_app/common/timers/pomodoro_state_machine/states/paused.gd
@@ -1,3 +1,4 @@
+class_name PausedState
 extends State
 
 
