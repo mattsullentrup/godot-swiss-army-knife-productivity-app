9ce8c652d6cd88ea9aa2590459a94601e6285a10
mattsullentrup <msullentrup1@gmail.com>
2024-09-06 20:07:00 -0500

    Update idle.gd
    Add all button type functionality to idle state

--- productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
+++ productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
@@ -19,3 +19,15 @@ func _on_button_pressed(button: int) -> void:
 				finished.emit("Work")
 			else:
 				finished.emit("Break")
+		ButtonTypes.SKIP:
+			if state_machine.productivity_state == ProductivityStates.BREAK:
+				state_machine.productivity_state = ProductivityStates.WORK
+				state_machine.current_round += 1
+			else:
+				state_machine.productivity_state = ProductivityStates.BREAK
+		ButtonTypes.GO_BACK:
+			if state_machine.productivity_state == ProductivityStates.BREAK:
+				state_machine.productivity_state = ProductivityStates.WORK
+			else:
+				state_machine.productivity_state = ProductivityStates.BREAK
+				state_machine.current_round -= 1
