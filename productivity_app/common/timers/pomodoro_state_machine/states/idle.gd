499cb180dd0a731add19a2cf65452ec564603e3e
mattsullentrup <msullentrup1@gmail.com>
2024-09-20 18:49:37 -0500

    Update idle.gd
    Remove check for break productivity state. Idle should only ever be in break state so it can transition to work when started

--- productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
+++ productivity_app/common/timers/pomodoro_state_machine/states/idle.gd
@@ -14,10 +14,8 @@ func _exit() -> void:
 func _on_button_pressed(button: int) -> void:
 	match button:
 		ButtonTypes.START:
-			if state_machine.productivity_state == ProductivityStates.BREAK:
-				finished.emit("Work")
-			else:
-				finished.emit("Break")
+			finished.emit("Work")
+			state_machine.productivity_state = ProductivityStates.WORK
 		ButtonTypes.SKIP:
 			if state_machine.productivity_state == ProductivityStates.BREAK:
 				state_machine.productivity_state = ProductivityStates.WORK
