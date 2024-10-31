8979ca1b59d4dd417f332a06c7f95f03fcf61e07
mattsullentrup <msullentrup1@gmail.com>
2024-10-30 19:44:37 -0500

    Update task_container.gd
    Make method to move child to mouse position reusable

--- productivity_app/project_elements/project/task_container.gd
+++ productivity_app/project_elements/project/task_container.gd
@@ -1,6 +1,8 @@
 extends VBoxContainer
 
 
+const MAX_TASKS = 100
+
 @export var task: PackedScene
 @export var toggle_tasks_button: Button
 @export var folded_icon: CompressedTexture2D
@@ -8,8 +10,10 @@ extends VBoxContainer
 
 var are_children_visible := true
 var separator: HSeparator
+var task_height: float
 
 
+#region Drag and drop behaviour
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	if separator == null:
 		return data is Task
@@ -18,20 +22,12 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 		return false
 
 	var data_node: Node = data
-	var task_size: float = data_node.size.y
-	var child_hovered_over := _get_child_dropped_on(task_size, at_position.y)
+	task_height = data_node.size.y
+	var child_hovered_over := _get_child_under_mouse(task_height, at_position.y)
 	if child_hovered_over == null:
 		return data is Task
 
-	var new_index: int = child_hovered_over.get_index()
-	var child_y_mid_point: float = child_hovered_over.position.y + task_size / 2
-	if separator.get_index() > new_index and child_y_mid_point < at_position.y:
-		new_index += 1
-	elif separator.get_index() < new_index and child_y_mid_point > at_position.y:
-		new_index -= 1
-
-	new_index = clampi(new_index, 0, 100)
-	move_child(separator, new_index)
+	_move_child_to_new_index(child_hovered_over, separator, at_position)
 
 	return data is Task
 
@@ -40,41 +36,47 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 	if separator != null:
 		separator.queue_free()
 
-	if data is not Node: return
+	if data is not Node:
+		return
 
 	var data_node: Node = data
-	var task_size: float = data_node.size.y
-	var child_dropped_on := _get_child_dropped_on(task_size, at_position.y)
-	if child_dropped_on == null or child_dropped_on == data_node: return
+	var child_dropped_on := _get_child_under_mouse(task_height, at_position.y)
+	if child_dropped_on == null:
+		return
 
 	if data_node not in get_children():
 		data_node.get_parent().remove_child(data_node)
 		add_child(data_node)
 
-	# get new index relative to child dropped on
-	var new_index: int = child_dropped_on.get_index()
-	var data_index: int = data_node.get_index()
-	var child_y_mid_point: float = child_dropped_on.position.y + task_size / 2
-	if data_index > new_index and child_y_mid_point < at_position.y:
+	_move_child_to_new_index(child_dropped_on, data_node, at_position)
+
+
+func _move_child_to_new_index(child_under_mouse: Node, node_to_move: Node, at_position: Vector2) -> void:
+	var new_index: int = child_under_mouse.get_index()
+	var current_index: int = node_to_move.get_index()
+	var child_y_mid_point: float = child_under_mouse.position.y + task_height / 2
+	if current_index > new_index and child_y_mid_point < at_position.y:
 		new_index += 1
-	elif data_index < new_index and child_y_mid_point > at_position.y:
+	elif current_index < new_index and child_y_mid_point > at_position.y:
 		new_index -= 1
 
-	new_index = clampi(new_index, 0, 100)
-	move_child(data_node, new_index)
+	new_index = clampi(new_index, 0, MAX_TASKS)
+	move_child(node_to_move, new_index)
 
 
-func _get_child_dropped_on(task_size: float, at_position_vertical: float) -> Node:
+func _get_child_under_mouse(task_size: float, at_position_vertical: float) -> Node:
 	for child in get_children():
-		if separator != null and child == separator:
+		if child == separator:
 			continue
 
 		if child.position.y + task_size > at_position_vertical:
 			return child
 
 	return null
+#endregion
 
 
+#region Task management
 func create_new_task(_unnecessary_text: String = "") -> void:
 	var new_task: Task = task.instantiate()
 	add_child(new_task)
@@ -108,3 +110,4 @@ func _on_reset_button_pressed() -> void:
 
 func _on_line_edit_text_submitted(_new_text: String) -> void:
 	create_new_task()
+#endregion
