d4120bf76f82aaf935a7de6ec9e44ee56d7ab79b
mattsullentrup <msullentrup1@gmail.com>
2024-11-04 21:13:31 -0600

    Nullify hovered child and separator on project manager mouse exited

--- productivity_app/to_do_item/project_manager.gd
+++ productivity_app/to_do_item/project_manager.gd
@@ -16,3 +16,11 @@ func _on_new_project_button_pressed() -> void:
 func _create_new_project() -> void:
 	var new_project: VBoxContainer = project.instantiate()
 	add_child(new_project)
+
+
+func _on_mouse_exited() -> void:
+	if not Rect2(Vector2(), size).has_point(get_local_mouse_position()):
+		#print("mouse exited")
+		child_hovered_over = null
+		if separator.is_inside_tree():
+			separator.get_parent().remove_child(separator)
