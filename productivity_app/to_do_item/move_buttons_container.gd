52ff1f54aeee9d16621282825f28fc84283b5757
mattsullentrup <msullentrup1@gmail.com>
2024-11-10 19:26:55 -0600

    Connect texture rect gui signals

--- productivity_app/to_do_item/move_buttons_container.gd
+++ productivity_app/to_do_item/move_buttons_container.gd
@@ -1,1 +1,20 @@
 extends VBoxContainer
+
+
+@onready var _up_texture_rect: TextureRect = $UpTextureRect
+@onready var _down_texture_rect: TextureRect = $DownTextureRect
+
+
+func _on_texture_rect_gui_input(event: InputEvent, texture_path: NodePath) -> void:
+	var click := event as InputEventMouseButton
+	if click:
+		pass
+
+
+func _on_texture_rect_mouse_entered(texture_path: NodePath) -> void:
+	var texture: TextureRect = get_node(texture_path)
+	print(texture)
+
+
+func _on_texture_rect_mouse_exited(texture_path: NodePath) -> void:
+	pass
