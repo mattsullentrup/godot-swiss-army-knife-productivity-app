5fa451747c261f23a2ca7607a22ca58eef5bd6a8
mattsullentrup <msullentrup1@gmail.com>
2024-11-11 11:56:42 -0600

    Fix new project shortcut

--- productivity_app/gui/drag_and_drop_container.gd
+++ productivity_app/gui/drag_and_drop_container.gd
@@ -1,74 +1,73 @@
-class_name DragAndDropContainer
+class_name ToDoItemContainer
 extends VBoxContainer
 ## Parent to child to do items and manages drag and drop behaviour
 
 
-const MAX_TASKS = 100
-
-@export_enum("Project", "Task", "SubTask") var _child_type: String = "Project"
-
-var task_height: float
-#static var child_hovered_over: Node
-static var separator: HSeparator
-
-
-func _ready() -> void:
-	if separator == null:
-		separator = HSeparator.new()
-
-
-func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	if data is not Node:
-		return false
-
-	var data_node: Node = data
-	var is_child_type := _is_node_child_type(data_node)
-	if not is_child_type:
-		return false
-
-	task_height = data_node.size.y
-	if ToDoItem.item_hovered_over == null:
-		return false
-
-	if not separator.get_parent() == self:
-		@warning_ignore("standalone_ternary")
-		add_child(separator) if separator.get_parent() == null else separator.reparent(self)
-
-	if is_child_type:
-		_move_child_to_new_index(ToDoItem.item_hovered_over, separator, at_position)
-
-	return is_child_type
-
-
-func _drop_data(at_position: Vector2, data: Variant) -> void:
-	separator.get_parent().remove_child(separator)
-	if ToDoItem.item_hovered_over == null:
-		return
-
-	if data is not Node:
-		return
-
-	var data_node: Node = data
-
-	if not is_ancestor_of(data_node):
-		data_node.reparent(self)
-
-	_move_child_to_new_index(ToDoItem.item_hovered_over, data_node, at_position)
-
-
-func _move_child_to_new_index(
-		child_under_mouse: Node, node_to_move: Node, at_position: Vector2
-) -> void:
-	var new_index: int = child_under_mouse.get_index()
-	var current_index: int = node_to_move.get_index()
-	var child_y_mid_point: float = child_under_mouse.position.y + task_height / 2
-	if current_index > new_index and child_y_mid_point < at_position.y:
-		new_index += 1
-	elif current_index < new_index and child_y_mid_point > at_position.y:
-		new_index -= 1
-
-	new_index = clampi(new_index, 0, MAX_TASKS)
-	move_child(node_to_move, new_index)
+#const MAX_CHILDREN = 50
+#
+#@export_enum("Project", "Task", "SubTask") var _child_type: String = "Project"
+#
+#var task_height: float
+#static var separator: HSeparator
+#
+#
+#func _ready() -> void:
+	#if separator == null:
+		#separator = HSeparator.new()
+#
+#
+#func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	#if data is not Node:
+		#return false
+#
+	#var data_node: Node = data
+	#var is_child_type := _is_node_child_type(data_node)
+	#if not is_child_type:
+		#return false
+#
+	#task_height = data_node.size.y
+	#if ToDoItem.item_hovered_over == null:
+		#return false
+#
+	#if not separator.get_parent() == self:
+		#@warning_ignore("standalone_ternary")
+		#add_child(separator) if separator.get_parent() == null else separator.reparent(self)
+#
+	#if is_child_type:
+		#_move_child_to_new_index(ToDoItem.item_hovered_over, separator, at_position)
+#
+	#return is_child_type
+#
+#
+#func _drop_data(at_position: Vector2, data: Variant) -> void:
+	#separator.get_parent().remove_child(separator)
+	#if ToDoItem.item_hovered_over == null:
+		#return
+#
+	#if data is not Node:
+		#return
+#
+	#var data_node: Node = data
+#
+	#if not is_ancestor_of(data_node):
+		#data_node.reparent(self)
+#
+	#_move_child_to_new_index(ToDoItem.item_hovered_over, data_node, at_position)
+#
+#
+#func _move_child_to_new_index(
+		#child_under_mouse: Node, node_to_move: Node, at_position: Vector2
+#) -> void:
+	#var new_index: int = child_under_mouse.get_index()
+	#var current_index: int = node_to_move.get_index()
+	#var child_y_mid_point: float = child_under_mouse.position.y + task_height / 2
+	#if current_index > new_index and child_y_mid_point < at_position.y:
+		#new_index += 1
+	#elif current_index < new_index and child_y_mid_point > at_position.y:
+		#new_index -= 1
+#
+	#new_index = clampi(new_index, 0, MAX_CHILDREN)
+	#move_child(node_to_move, new_index)
 #
 #
 #func _get_child_under_mouse(at_position_y: float) -> Node:
@@ -83,6 +82,6 @@ func _move_child_to_new_index(
 	#return null
 
 
-func _is_node_child_type(node: Node) -> bool:
-	var type: StringName = node.get_script().get_global_name()
-	return type == _child_type
+#func _is_node_child_type(node: Node) -> bool:
+	#var type: StringName = node.get_script().get_global_name()
+	#return type == _child_type
--- productivity_app/to_do_item/project_manager.gd
+++ productivity_app/to_do_item/project_manager.gd
@@ -1,30 +1,7 @@
-extends DragAndDropContainer
+extends ToDoItemContainer
 
 
 @export var project: PackedScene
-
-
-func _gui_input(event: InputEvent) -> void:
-	var mouse := event as InputEventMouseMotion
-	if mouse == null:
-		return
-
-	var item := ToDoItem.item_hovered_over
-	if item == null:
-		return
-
-	var item_mouse_pos := item.get_local_mouse_position()
-
-	#NOTE: This doesn't work but I'm trying to make it so if the mouse is in the gap between tasks
-	# it doesn't stop being the hovered item for those few pixels
-	# This is so fuckin stupid there's gotta be a better way to deal with this
-	if not Rect2(Vector2(0, -5), Vector2(item.size.x, item.size.y + 5)).has_point(item_mouse_pos):
-		ToDoItem.item_hovered_over = null
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("new_project"):
-		_create_new_project()
 
 
 func _on_new_project_button_pressed() -> void:
@@ -34,12 +11,3 @@ func _on_new_project_button_pressed() -> void:
 func _create_new_project() -> void:
 	var new_project: VBoxContainer = project.instantiate()
 	add_child(new_project)
-
-
-func _on_mouse_exited() -> void:
-	if not Rect2(Vector2(), size).has_point(get_local_mouse_position()):
-		#ToDoItem.item_hovered_over = null
-		print("mouse exited")
-		#child_hovered_over = null
-		#if separator.is_inside_tree():
-			#separator.get_parent().remove_child(separator)
