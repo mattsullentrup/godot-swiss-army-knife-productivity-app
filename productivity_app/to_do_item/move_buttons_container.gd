a5c3043763bd755ef349ea4091f8940ac08e3738
mattsullentrup <msullentrup1@gmail.com>
2024-11-10 19:34:26 -0600

    Update move_buttons_container.gd
    Get move buttons working

--- productivity_app/to_do_item/move_buttons_container.gd
+++ productivity_app/to_do_item/move_buttons_container.gd
@@ -3,12 +3,20 @@ extends VBoxContainer
 
 @onready var _up_texture_rect: TextureRect = $UpTextureRect
 @onready var _down_texture_rect: TextureRect = $DownTextureRect
+@onready var _owner := get_owner()
+@onready var _owner_parent := _owner.get_parent()
 
 
 func _on_texture_rect_gui_input(event: InputEvent, texture_path: NodePath) -> void:
 	var click := event as InputEventMouseButton
-	if click:
-		pass
+	if click and click.pressed:
+		var texture: TextureRect = get_node(texture_path)
+		if texture == _up_texture_rect:
+			if not _owner.get_index() == 0:
+				_owner_parent.move_child(_owner, _owner.get_index() - 1)
+		elif texture == _down_texture_rect:
+			if not _owner.get_index() == _owner_parent.get_child_count() - 1:
+				_owner_parent.move_child(_owner, _owner.get_index() + 1)
 
 
 func _on_texture_rect_mouse_entered(texture_path: NodePath) -> void:
@@ -17,4 +25,5 @@ func _on_texture_rect_mouse_entered(texture_path: NodePath) -> void:
 
 
 func _on_texture_rect_mouse_exited(texture_path: NodePath) -> void:
+	var texture: TextureRect = get_node(texture_path)
 	pass
