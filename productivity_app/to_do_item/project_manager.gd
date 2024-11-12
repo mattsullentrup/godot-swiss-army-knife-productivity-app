5dd068c55033df6e20e6ad7f79bab872ad3537b2
mattsullentrup <msullentrup1@gmail.com>
2024-11-12 16:39:57 -0600

    Add shortcut for new task and sub task when nothing is focused

--- productivity_app/to_do_item/project_manager.gd
+++ productivity_app/to_do_item/project_manager.gd
@@ -1,24 +1,21 @@
 extends VBoxContainer
 
 
-@export var project: PackedScene
+@export var project_scene: PackedScene
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	# TODO: If nothing is focused or focused node is a to do item,
 	# get last task/sub_task and add new item below it
-	#super(event)
 	var focused := get_viewport().gui_get_focus_owner()
-	if focused and is_ancestor_of(focused):
+	if (focused and is_ancestor_of(focused)) or get_child_count() == 0:
 		return
 
 	if event.is_action_pressed("new_task"):
-		var last_project: Project = get_child(-1)
-		if last_project:
-			last_project.create_new_task()
-			get_viewport().set_input_as_handled()
-	if event.is_action_pressed("new_sub_task"):
-		pass
+		_create_lowest_task()
+	elif event.is_action_pressed("new_sub_task"):
+		_create_lowest_sub_task()
+
 
 func _process(_delta: float) -> void:
 	var focused := get_viewport().gui_get_focus_owner()
@@ -26,10 +23,30 @@ func _process(_delta: float) -> void:
 		print(focused)
 
 
+func _create_lowest_task() -> void:
+	var last_project: Project = get_child(-1)
+	last_project.create_new_task()
+	get_viewport().set_input_as_handled()
+
+
+func _create_lowest_sub_task() -> void:
+	var children := get_children()
+	children.reverse()
+	for child in children:
+		var task_container: VBoxContainer = child.get_node("%TaskContainer")
+		if task_container.get_child_count() == 0:
+			continue
+
+		var last_task: Task = task_container.get_child(-1)
+		last_task.create_new_task()
+
+	get_viewport().set_input_as_handled()
+
+
 func _on_new_project_button_pressed() -> void:
 	_create_new_project()
 
 
 func _create_new_project() -> void:
-	var new_project: VBoxContainer = project.instantiate()
+	var new_project: VBoxContainer = project_scene.instantiate()
 	add_child(new_project)
