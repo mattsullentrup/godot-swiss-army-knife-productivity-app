9ac7e96c7bc526f0129581b63e193d323e0d670d
mattsullentrup <msullentrup1@gmail.com>
2024-11-12 18:21:15 -0600

    Add save settings for autopan

--- productivity_app/common/audio/effects/autopan.gd
+++ productivity_app/common/audio/effects/autopan.gd
@@ -1,34 +1,44 @@
 extends HBoxContainer
 
 
+const PANNER_INDEX = 1
+
 @export var pan_speed: float = 0.0005
 
 var can_autopan := false
-var panner_index: int = 1
 
 @onready var effects_bus_index := AudioServer.get_bus_index("Effects")
 @onready var noise_bus_index := AudioServer.get_bus_index("Noise")
-@onready var panner := AudioServer.get_bus_effect(effects_bus_index, 1) as AudioEffectPanner
+@onready var panner: AudioEffectPanner = AudioServer.get_bus_effect(effects_bus_index, PANNER_INDEX)
 
 
-func _ready() -> void:
-	AudioServer.set_bus_effect_enabled(effects_bus_index, panner_index, false)
-	pan_speed = $AutopanSlider.value
-
-
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if can_autopan and not AudioServer.is_bus_mute(noise_bus_index):
 		if panner.pan > 1:
 			pan_speed = -pan_speed
 		if panner.pan < -1:
 			pan_speed = abs(pan_speed)
-		panner.pan += pan_speed
+		panner.pan += pan_speed * delta
 		clampf(panner.pan, -1, 1)
 
 
+func save(save_file: SaveFile) -> void:
+	save_file.autopan_value = abs(pan_speed)
+	save_file.autopan_toggled_on = can_autopan
+
+
+func load(save_file: SaveFile) -> void:
+	pan_speed = save_file.autopan_value
+	#can_autopan = save_file.autopan_toggled_on
+
+	#AudioServer.set_bus_effect_enabled(effects_bus_index, PANNER_INDEX, can_autopan)
+	$AutopanCheckBox.button_pressed = save_file.autopan_toggled_on
+	$AutopanSlider.value = pan_speed
+
+
 func _on_autopan_check_button_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_effect_enabled(effects_bus_index, panner_index, toggled_on)
-	toggled_on = not toggled_on
+	AudioServer.set_bus_effect_enabled(effects_bus_index, PANNER_INDEX, toggled_on)
+	#toggled_on = not toggled_on
 	can_autopan = not can_autopan
 	if can_autopan == false:
 		panner.pan = 0
--- productivity_app/config/save/save_file.gd
+++ productivity_app/config/save/save_file.gd
@@ -4,4 +4,6 @@ extends Resource
 
 @export var projects_data: Array[ProjectData]
 @export var noise_volume: float = 0.25
+@export var autopan_value: float = 0.1
+@export var autopan_toggled_on := false
 @export var pomodoro_data: Dictionary
